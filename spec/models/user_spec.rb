3579ed9110c90caa3db2942384477bb31614d977
iwasawa15 <tatsuya57sak@gmail.com>
2019-02-07 18:20:35 +0900

    test email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,11 @@ describe User do
      user.valid?
      expect(user.errors[:nickname]).to include("can't be blank")
     end
+
+    it "is invalid without an email" do
+      user = User.new(nickname: "aaa", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
   end
 end
