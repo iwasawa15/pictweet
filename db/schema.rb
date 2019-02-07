3bd5e66df3bcf20e53a070e519b05e7e69c4c596
iwasawa15 <tatsuya57sak@gmail.com>
2019-02-07 15:57:59 +0900

    geminstall and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20180716184109) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "nickname",               limit: 255
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
@@ -43,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180716184109) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
